fbd25f073653608ceb933e284e48880a3d2f3d91
Sam Jones <sam@imnotpete.com>
2020-10-03 14:00:38 -0400

    parsing date from query string; displaying default page otherwise

--- js/day1of.js
+++ js/day1of.js
@@ -1,3 +1,48 @@
+const DATE_PARAM = "startDate";
+const LocalDate = JSJoda.LocalDate;
+const currentDate = LocalDate.now();
+var startDate;
+
 $(document).ready(function() {
-	console.log("ready");
+	let valid = parseParams();
+
+	if (valid) {
+		displayDayOf();
+	} else {
+		displaySelection();
+	}
+
 });
+
+function parseParams() {
+	let searchString = window.location.search;
+	let searchParams = new URLSearchParams(searchString);
+
+	if (! searchParams.has(DATE_PARAM)) {
+		return false;
+	}
+
+	let dateString = searchParams.get(DATE_PARAM)
+
+	try {
+		startDate = LocalDate.parse(dateString);
+	} catch (e) {
+		console.error(e);
+		return false;
+	}
+
+	if (startDate.isAfter(currentDate)) {
+		return false;
+	}
+
+	return true;
+}
+
+function displayDayOf() {
+	$("#startDate").text(startDate);
+	$("#startDate").show();
+}
+
+function displaySelection() {
+	$("#selectDate").show();
+}
